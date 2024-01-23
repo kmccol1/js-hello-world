e7b64435e103e23856914e4fa3ddb820c3ced2d2
Kyle McColgan <kdmbus21@gmail.com>
2024-01-22 18:18:44 -0600

    Update hello world.

--- hello.js
+++ hello.js
@@ -4,19 +4,30 @@ Enter your code on line 7 to print 'Hello, World!'
 to the console (the panel on the right)
 */
 
-console.log("Hello");
+console.log("Hello, World!");
 
 /*  
 Next, PLAY! 
 Modify your code on line 7 to try to accomplish the tasks listed below:
 
    1. Change the message that is printed.
+      console.log("Hello to the World!");
    2. Figure out what the parentheses do. Will the code work without them?
+      console.log"Hello to the World!";
    3. Remove one or both quotation marks. Do we need to include both opening and closing quote marks? Is there a difference between using a single or a double quote (' vs. ")?
+       console.log('Hello to the World!');
    4. Remove the semi-colon, ;.
+      console.log("Hello to the World!")
    5. Print a number. (Bonus: Print two numbers added together).
+      console.log(4);
+      console.log(8+8);
    6. Print multiple messages one after the other.
+       console.log("Hello to the World!");
+       console.log("World!");
    7. Print two messages on the same line.
+       console.log("Hello", "World!");
    8. Print a message that contains quote marks, such as Quoth the Raven "Nevermore".
+       console.log('Quoth the Raven "Nevermore"');
    9. Other. You choose!
+       console.log('"The world is quiet here."');
 */
